9f83f2194bccd1ba223a1d1198939f4c3d80d1f2
Jerald Sheets <questy@gmail.com>
2014-04-10 15:28:17 -0500

    cleaning up manifest

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,81 +6,8 @@ class coa-apache::config {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd.conf',
+    source  => 'puppet:///modules/coa-apache/httpd.conf',
     require => Class['coa-apache::install'],
     notify  => Class['coa-apache::service'],
-  }
-
-  # Main Server Configuration elements
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-default.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-default.conf',
-    require  => Class['coa-apache::install'],
-    notify   => Class['coa-apache::service'],
-  }
-
-  # Apache Error Documents Declaration
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-errordoc.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-errordoc.conf',
-    require  => Class['coa-apache::install'],
-    notify   => Class['coa-apache::service'],
-  }
-
-  # Apache Server Info & Status Module Settings
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-info.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-info.conf',
-    require  => Class['coa-apache::install'],
-    notify   => Class['coa-apache::service'],
-  }
-
-  # Apache Prefork/MPM Configuration Section
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-mpm.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-mpm.conf',
-    require  => Class['coa-apache::install'],
-    notify   => Class['coa-apache::service'],
-  }
-
-  # "Placeholder" Vhosts setting
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-vhosts.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-vhosts.conf',
-    require  => Class['coa-apache::install'],
-    notify   => Class['coa-apache::service'],
-  }
-
-  file { '/etc/httpd/conf/etc_httpd_conf_httpd-autoindex.conf':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/etc_httpd_conf_httpd-autoindex.conf',
-    require  => Class['coa-apache::install'],
-  }
-
-  file { '/etc/httpd/conf/mime.types':
-    ensure   => present,
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    source   => 'puppet:///modules/coa-apache/mime.types',
-    require  => Class['coa-apache::install'],
   }
 }
